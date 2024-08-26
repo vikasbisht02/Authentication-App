6bfc9f57ff2e2bd0c6659001e9c6e57548c6abf6
Vikas Bisht <vikasbisht732002@gmail.com>
2024-08-26 07:00:50 -0500

    server updated

--- server/index.js
+++ server/index.js
@@ -13,8 +13,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({
+  origin: ["http://localhost:5173", "https://authentication-app-eight-jet.vercel.app"],
+  credentials: true,
+}));
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json()); // allows us to parse incoming requests:req.body
 app.use(cookieParser()); // allows us to parse incoming cookies
